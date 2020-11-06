8236e8801511831db4dc31bf2f50870736c5903c
Michael Black <michael.black@trinsic.id>
2020-11-05 17:38:59 -0700

    installed vue; started making vue components

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark position-fixed fixed-top">
+        <a class="navbar-brand" href="/">SJB</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="/portfolio">Portfolio</a>
+                <a class="nav-item nav-link" href="/scheduling">Schedule/Pricing</a>
+                <a class="nav-item nav-link" href="/about">About</a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+  }
+}
+</script>
